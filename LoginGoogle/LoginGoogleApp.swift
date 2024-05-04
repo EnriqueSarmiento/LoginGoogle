a291dc9f596df55a2a8ef37b95b4a1abde779030
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-05-04 09:10:01 -0400

    Initial Commit

---
+++ LoginGoogle/LoginGoogleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginGoogleApp.swift
+//  LoginGoogle
+//
+//  Created by Enrique Sarmiento on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginGoogleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
