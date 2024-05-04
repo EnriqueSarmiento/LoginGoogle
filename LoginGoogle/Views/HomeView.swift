d451fc0a3055c839df3c080bf492dc1c72902f73
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-05-04 10:54:15 -0400

    feature: - Added some styles for home view

--- LoginGoogle/Views/HomeView.swift
+++ LoginGoogle/Views/HomeView.swift
@@ -6,9 +6,37 @@
 //
 
 import SwiftUI
+import GoogleSignIn
 
 struct HomeView: View {
+   
+   @EnvironmentObject var viewModel: AuthViewModel
+   /** this variable will help to get user information */
+   let user = GIDSignIn.sharedInstance.currentUser
+   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       NavigationView{
+          VStack(alignment: .center){
+             AsyncImage(url: user?.profile?.imageURL(withDimension: 200))
+             
+             Text(user?.profile?.name ?? "")
+                .font(.headline)
+             
+             Text(user?.profile?.email ?? "")
+                .font(.subheadline)
+
+             Spacer()
+             
+             Button {
+                viewModel.signOut()
+             } label: {
+                Text("salir")
+                
+             }.buttonStyle(BorderedButtonStyle())
+                .tint(.red)
+
+          }.navigationTitle("Home")
+             .padding(.all)
+       }
     }
 }
--- LoginGoogle/Views/LoginView.swift
+++ LoginGoogle/Views/LoginView.swift
@@ -8,7 +8,23 @@
 import SwiftUI
 
 struct LoginView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+   
+   @EnvironmentObject var viewModel: AuthViewModel
+   
+   var body: some View {
+      VStack(alignment: .center){
+         
+         Text("Login with Google")
+            .fontWeight(.black)
+            .foregroundColor(.red)
+            .font(.largeTitle)
+         GoogleButtonView()
+            .frame(height: 50,alignment: .center)
+            .onTapGesture {
+               viewModel.signIn()
+            }
+         
+      }.padding(.all)
+      
+   }
 }
